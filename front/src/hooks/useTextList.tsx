0c4e3a9c2df9c634f90c5c9ba32ff5e0e4710e22
nakki <rakki0404@gmail.com>
2023-12-03 18:44:38 +0900

    textList type fixed

--- front/src/hooks/useTextList.tsx
+++ front/src/hooks/useTextList.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { useTextListQuery } from "@/hooks/api/useTextListQuery";
+import type { Text } from "@/types/Text";
 
 export function useTextList() {
   const { TextList } = useTextListQuery();
 
-  const [textList, setTextList] = useState([]);
+  const [textList, setTextList] = useState<Text[]>([]);
   useEffect(() => {
     if (TextList) {
       setTextList(TextList);
