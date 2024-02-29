cb64ef1b8c4aabaade3f709879615f89570abb08
nakki <rakki0404@gmail.com>
2024-03-01 04:33:24 +0900

    cors

--- front/src/components/ChatBot.tsx
+++ front/src/components/ChatBot.tsx
@@ -27,7 +27,7 @@ export default function ChatBot() {
     !active && setActive(!active);
 
     if (!isConnected) {
-      const socketInstance = io("http://localhost:3001", {
+      const socketInstance = io("http://www.nextriceark.store", {
         withCredentials: true,
       });
       socketInstance.on("connect", async () => {});
