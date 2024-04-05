9ddb05599a18ed353f8a4346da7185e6dd3e4ec2
nakki <rakki0404@gmail.com>
2024-04-06 01:55:37 +0900

    Fix:Front Build Error

--- front/src/components/Ex/Ex.tsx
+++ front/src/components/Ex/Ex.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-const axios = require("axios");
+// const axios = require("axios");
 
 export default function Ex() {
   return (
