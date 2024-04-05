57a030826b0c95866d5fc72270330246aa845e47
nakki <rakki0404@gmail.com>
2024-04-06 02:12:53 +0900

    Fix:502 Error

--- front/src/components/Ex/Ex.tsx
+++ front/src/components/Ex/Ex.tsx
@@ -2,22 +2,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-// const axios = require("axios");
 
 export default function Ex() {
-  return (
-    <div>
-      {/*
-      아이콘 탐색 코드.
-      {Array.from({ length: 14 }, (_, i) =>
-        Array.from({ length: 256 }, (_, j) => (
-          <img
-            key={`image_${i}_${j}`}
-            src={`https://cdn-lostark.game.onstove.com/efui_iconatlas/use/use_${i}_${j}.png`}
-            alt={`아이템 이미지 ${i}_${j}`}
-          />
-        ))
-      )} */}
-    </div>
-  );
+  return <div></div>;
 }
